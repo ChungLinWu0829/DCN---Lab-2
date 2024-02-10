ce796882e1590d658bd160cb62ece276ec785389
ChungLinWu0829 <mk5622521202001@gmail.com>
2024-02-10 16:28:39 -0500

    add current_time function

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+
+@app.route('/time/')
+def current_time():
+    current_date_and_time = datetime.now()
+    return 'The current time is {:%b %d, %Y %H:%M:%S}'.format(current_date_and_time)
 
 
 app.run(host='0.0.0.0',
